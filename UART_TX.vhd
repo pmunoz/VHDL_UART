94a9f89edf33c97e59365f1adb18b7a950785a68
Pablo Muñoz <1078066+pmunoz@users.noreply.github.com>
2019-06-16 13:45:56 +0200

    Add an initial version for the UART transmitter.

--- UART_TX.vhd
+++ UART_TX.vhd
@@ -2,12 +2,12 @@
 --| Author: Pablo Muñoz Galindo 
 --|
 --| Project Name:   VHDL Simple UART
---| Module Name:    UART_RX - Behavioral
---| Description:    UART receiver (includes state machine to control block's 
+--| Module Name:    UART_TX - Behavioral
+--| Description:    UART transmitter (includes state machine to control block's 
 --|                 operation) 
 --|
---| Created:    	  17:43:08 06/04/2019
---| Tested using:   isim
+--| Created:    	  21:20:34 06/14/2019 
+--| Tested using:   untested
 --|
 --| (c)2019 Pablo Muñoz
 --| This code is licensed under MIT license (see LICENSE.md for details)
@@ -18,31 +18,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use IEEE.std_logic_unsigned.all;
 
-entity UART_RX is
+entity UART_TX is
     Port ( Clk : in  STD_LOGIC;	
 			  Reset : in STD_LOGIC;
-			  Serial_in : in STD_LOGIC;
-			  Data_out : out STD_LOGIC_VECTOR(7 downto 0);
-			  Correct_rx : out std_logic
+			  Write_en : in STD_LOGIC;
+			  Data_in : in STD_LOGIC_VECTOR(7 downto 0);
+			  Serial_out : out STD_LOGIC
 			  );
-end UART_RX;
+end UART_TX;
 
-architecture Behavioral of UART_RX is
+architecture Behavioral of UART_TX is
 
 ------------------------------------------------------------------------
--- Components for UART receiver
+-- Components for UART transmitter
 ------------------------------------------------------------------------
-component Shift_Registry_8bit_counter is
-    Port ( Clk : in  STD_LOGIC;
-           CLR : in  STD_LOGIC;
-			  Hold : in STD_LOGIC;
-           Ser_in : in  STD_LOGIC;
-			  Carry_out : out STD_LOGIC;
-           Par_out : out  STD_LOGIC_VECTOR (7 downto 0);
-			  Counter_out : out STD_LOGIC_VECTOR (7 downto 0)
-	 );
-end component;
-
 component Registry_8bit is
     Port ( Clk : in STD_LOGIC;
 			  Reset : in STD_LOGIC;
@@ -54,7 +43,7 @@ end component;
 ------------------------------------------------------------------------
 -- Type definitions
 ------------------------------------------------------------------------
-TYPE state IS (standby, startBit, waitForBit, sampleBit, stopAndStore); -- definition of states for the FSM
+TYPE state IS (standby, startBit, txDataBit, incPointer, stopBit); -- definition of states for the FSM
 
 ------------------------------------------------------------------------
 -- Internal signals
@@ -73,44 +62,31 @@ signal TIMER_BIT_reset : std_logic := '1';
 
 signal Clk_FSM : std_logic := '0';
 
-signal ShiftReg_capture : std_logic := '0';
-signal ShiftReg_hold : std_logic := '0';
-signal ShiftReg_reset : std_logic := '1';
-signal ShiftReg_data_out : std_logic_vector(7 downto 0);
-signal ShiftReg_bitcount : std_logic_vector(7 downto 0);
-signal stopBit : std_logic := '0';
-
 signal Reg_capture : std_logic := '0';
-signal Reg_reset : std_logic := '1';
+--signal Reg_reset : std_logic := '1';
 signal Reg_input : std_logic_vector (7 downto 0);
 signal Reg_output : std_logic_vector (7 downto 0);
 
---signal par_data : std_logic_vector(7 downto 0) := x"00";
+signal bit_pointer : unsigned(4 downto 0):= "00000";
+
+signal output_bs_D : std_logic := '1';
+signal output_bs_Q : std_logic := '1';
+signal output_bs_Clk : std_logic := '0'; 
 
 begin
 
--- Shift registry for UART
-SHIFT_REGISTRY : Shift_Registry_8bit_counter
-    port map (
-		Clk => ShiftReg_capture,
-		CLR => ShiftReg_reset,
-		Hold => ShiftReg_hold,
-		Ser_in => Serial_in,
-		Carry_out => open,
-		Par_out => ShiftReg_data_out,
-		Counter_out => ShiftReg_bitcount);
-		
--- Output registry
-OUTPUT_REGISTRY : Registry_8bit
+	
+-- Input registry
+INPUT_REGISTRY : Registry_8bit
 	port map (
 		Clk => Reg_capture,
 		Reset => Reset  ,
-		input => Reg_input,
+		input => Data_in,
 		output => Reg_output);
 
 -- Other connections
-Reg_input <= ShiftReg_data_out;
-Data_out <= Reg_output;
+--Reg_input <= ShiftReg_data_out;
+--Data_out <= Reg_output;
 
 -- Clock prescaler for state machine
 tmp_calc <= Freq_base_Clk_FPGA/(clk_multiplier*default_baudrate);
@@ -140,9 +116,7 @@ BIT_PERIOD_TIMER : process (Clk_FSM, TIMER_BIT_reset, TIMER_bit) is begin
 				TIMER_BIT_expired <= '0';
 			end if; 
 		end if;
-	end if;
-	
-	
+	end if;	
 end process;
 
 -- Status registry
@@ -156,63 +130,72 @@ STATE_REG : process (Clk_FSM) is begin
 	end if;
 end process;
 
+-- Output bistable
+OUTPUT_BS : process (output_bs_Clk) is begin
+	if rising_edge(output_bs_Clk) then
+		output_bs_Q <= output_bs_D;
+	end if;
+end process;
+
 -- Transition logic
-TRANSITION_LOG : process(current_state, Serial_in, TIMER_BIT_expired, Reset) is begin
+TRANSITION_LOG : process(current_state,Reset,TIMER_BIT_expired,Write_en) is begin
 	next_state <= current_state;
 	case current_state is
 		when standby =>
-			if Reset = '0' and falling_edge(Serial_in) then next_state <= startBit; 
+			if Reset = '0' and falling_edge(Write_en) then next_state <= startBit; 
 			end if;
 		when startBit =>
-				next_state <= waitForBit;
-		when waitForBit =>
 			if rising_edge(TIMER_BIT_expired) then 
-				if ShiftReg_bitcount < x"08" then
-					next_state <= sampleBit;
-			   else
-					next_state <= stopAndStore;
+				next_state <=  txDataBit;
+			end if;
+		when txDataBit =>
+			if rising_edge(TIMER_BIT_expired) then 
+				if bit_pointer < x"08" then
+					next_state <= incPointer;
+				else
+					next_state <= stopBit;
 				end if;
 			end if;
-		when sampleBit =>
-			next_state <= waitForBit; 
-      when stopAndStore =>
-			next_state <= standby;
+		when incPointer =>
+			next_state <=  txDataBit; 
+      when stopBit =>
+			if rising_edge(TIMER_BIT_expired) then 
+				next_state <= standby;
+			end if;
 	end case;
 end process;
 
 -- Output generation
-OUTPUT_GEN : process(current_state, Serial_in, Reset,ShiftReg_data_out, ShiftReg_bitcount) is begin
-	
-	ShiftReg_capture <= '0';
-	ShiftReg_hold <= '0';
-	ShiftReg_reset <= '0';
-	Correct_rx <= '0';
+OUTPUT_GEN : process(current_state,output_bs_Q,Reg_output,bit_pointer) is begin
 	TIMER_BIT_reset <= '1';
-	TIMER_BIT_threshold <= x"00";
+	TIMER_BIT_threshold <= x"00";	
 	Reg_capture <= '0';
+	--Reg_reset <= '0';
+	Serial_out <= '1';
+	output_bs_Clk <= '0';
+	output_bs_D <= '0';	
+	Serial_out <= output_bs_Q; 
 	
 	case current_state is
-		when standby =>
-			ShiftReg_hold <= '1';
-		when startBit =>
-			ShiftReg_reset <= '1';
---			TIMER_BIT_threshold <= clk_multiplier(8 downto 1);
---			TIMER_BIT_reset <= '0';
-		when waitForBit =>
-			if ShiftReg_bitcount = x"00" then
-				TIMER_BIT_threshold <= clk_multiplier(8 downto 1)+clk_multiplier(7 downto 0)-x"02";
-			else
-				TIMER_BIT_threshold <= clk_multiplier(7 downto 0)-x"02";
-			end if;
-			TIMER_BIT_reset <= '0';
-		when sampleBit =>
-			ShiftReg_capture <= '1';		
-      when stopAndStore =>
-			ShiftReg_hold <= '1';
-			if Serial_in = '1' then Correct_rx <= '1'; end if;
+		when standby =>				
+			--Reg_reset <= '1';
+		when startBit =>					
 			Reg_capture <= '1';
-	end case;
+			TIMER_BIT_threshold <= clk_multiplier(7 downto 0);
+			TIMER_BIT_reset <= '0';
+		when txDataBit =>
+			TIMER_BIT_threshold <= clk_multiplier(7 downto 0);
+			TIMER_BIT_reset <= '0';
+			output_bs_D <= Reg_output(to_integer(bit_pointer));			
+			output_bs_Clk <= '1';	
+		when incPointer =>
+			TIMER_BIT_reset <= '1';
+			bit_pointer<=bit_pointer+1;
+      when stopBit =>
+			Reg_capture <= '1';
+			TIMER_BIT_threshold <= clk_multiplier(7 downto 0);
+			TIMER_BIT_reset <= '0';
+	end case;		
 end process;
 
 end Behavioral;
-
